0e58483c16fbbd79ca2206aecdd4cbaf254cb11a
Amit Kumar <amitkumar655921@gmail.com>
2024-02-10 21:29:54 +0530

    making login/authorization feat

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,13 +1,36 @@
 const express = require("express");
-const { userModel } = require("../models/userModel");
+const { UserModel } = require("../models/userModel");
 
 const userRouter = express.Router();
 
-userRouter.get("/register", (req, res) => {
-  res.send({ message: "Register Page" });
+userRouter.post("/register", async (req, res) => {
+  const { name, gender, email, password } = req.body;
+  try {
+    const newUser = new UserModel({ email, password, gender, name });
+    await newUser.save();
+
+    res.status(201).send({ message: "new user has been created" });
+  } catch (error) {
+    res.status(400).send({ message: error });
+  }
 });
-userRouter.get("/login", (req, res) => {
-  res.send({ message: "login Page" });
+userRouter.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const dbUser = UserModel.find({ email });
+    console.log(password, dbUser.password);
+    if (dbUser) {
+      if (password == dbUser.password) {
+        res.status(200).send({ message: "Login successful" });
+      } else {
+        res.status(200).send({ message: "Wrong credentials" });
+      }
+    } else {
+      res.status(200).send({ message: "User not found" });
+    }
+  } catch (error) {
+    res.status(400).send({ message: error });
+  }
 });
 
 module.exports = { userRouter };
